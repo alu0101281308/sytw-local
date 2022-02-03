a771dba2c2b09f26abc0bcaa894d16cbb85a45f8
alu0101166247 <alu0101166247@ull.edu.es>
2022-02-03 21:34:40 +0000

    baterias

--- api/src/controllers/tiendaController.js
+++ api/src/controllers/tiendaController.js
@@ -3,7 +3,7 @@ const Bateria = require('../models/Bateria');
 exports.consultarBaterias = async (req, res) => {       
     try {         
         const baterias = await Bateria.find().sort({registro: -1});         
-        res.json(baterias);     
+        res.json({ baterias });     
     } catch (error) {         
         console.log(error);         
         res.status(500).send('Algo salio mal en obtener baterias de tienda');     
